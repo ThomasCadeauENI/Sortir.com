34593ca54c79037009f80f3464aa56b429d8829b
llebayon2020 <logan.lebayon2020@campus-eni.fr>
2021-10-18 17:25:58 +0200

    controller creer debut

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -6,13 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/sortie', 'sortie_')]
 class SortieController extends AbstractController
 {
-    #[Route('/sortie', 'sortie')]
-    public function index(): Response
-    {
-        return $this->render('sortie/index.html.twig', [
-            'controller_name' => 'SortieController',
-        ]);
+    #[Route('/', '')]
+    public function index(): Response {
+        return $this->render('sortie/index.html.twig');
+    }
+
+    #[Route('/creer', 'creer')]
+    public function creer_sortie(): Response {
+        return $this->render ('sortie/creer_sortie.html.twig');
     }
 }
