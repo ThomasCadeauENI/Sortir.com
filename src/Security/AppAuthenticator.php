50eeee9eb480bf674507ec65834a6267955479b0
tcadeau2020 <tcadeau@groupeais.fr>
2021-10-19 17:20:23 +0200

    Retour à la page d'acceuil quand connexion réussise.

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -89,12 +89,13 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
         return $credentials['password'];
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
 
+        return new RedirectResponse($this->urlGenerator->generate('index'));
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
